9a3a4b89ff5fabbb8755efa09d2dcea8624abe52
Pappol <riccardo.parol@gmail.com>
2023-02-07 16:46:42 +0100

    update

--- object_tracking.py
+++ object_tracking.py
@@ -20,7 +20,7 @@ def color_contorns(hsv, lower, upper, color):
     mask= cv2.inRange(hsv, lower, upper)
     #dilate the mask
     kernel = np.ones((5,5),np.uint8)
-    mask = cv2.dilate(mask, kernel, iterations = 1)
+    #mask = cv2.dilate(mask, kernel, iterations = 1)
     #find the contours
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # keep only 20% of the contours
@@ -31,13 +31,12 @@ def color_contorns(hsv, lower, upper, color):
     contours = [cnt for cnt in contours if circ < circularity(cnt)]
     #find the center of the contours
     for cnt in contours:
-        #filter for small areas
         M = cv2.moments(cnt)
         if M['m00'] != 0:
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             #draw a circle on the center of the contours
-            cv2.circle(frame, (cx, cy), 10, color, -1)
+            #cv2.circle(frame, (cx, cy), 10, color, -1)
         #draw the contours
         cv2.drawContours(frame, contours, -1, color, 3)
     return frame
@@ -48,31 +47,16 @@ cv2.namedWindow('image')
 cap = cv2.VideoCapture(0)
 
 cv2.createTrackbar('area','image',0,100, dummy)
-cv2.createTrackbar('length_u','image',0,400, dummy)
-cv2.createTrackbar('length_l','image',0,200, dummy)
+cv2.createTrackbar('length_u','image',300,1000, dummy)
+cv2.createTrackbar('length_l','image',0,400, dummy)
 cv2.createTrackbar('circularity','image',0,100, dummy)
 cv2.setTrackbarPos('area','image',20)
-cv2.setTrackbarPos('length_l','image',10)
-cv2.setTrackbarPos('length_u','image',30)
+cv2.setTrackbarPos('length_l','image',100)
+cv2.setTrackbarPos('length_u','image',100)
 cv2.setTrackbarPos('circularity','image',40)
 
-#create a filter for different colors
-#red
-cv2.createTrackbar('RMinH','image',0,255, dummy)
-cv2.createTrackbar('RMaxH','image',0,255, dummy)
-cv2.createTrackbar('RMinS','image',0,255, dummy)
-cv2.createTrackbar('RMaxS','image',0,255, dummy)
-cv2.createTrackbar('RMinV','image',0,255, dummy)
-cv2.createTrackbar('RMaxV','image',0,255, dummy)
-cv2.setTrackbarPos('RMinH','image',0)
-cv2.setTrackbarPos('RMaxH','image',20)
-cv2.setTrackbarPos('RMinS','image',130)
-cv2.setTrackbarPos('RMaxS','image',215)
-cv2.setTrackbarPos('RMinV','image',135)
-cv2.setTrackbarPos('RMaxV','image',255)
-RMinH = RMaxH = RMinS = RMaxS = RMinV = RMaxV = 0
-# lower_red = np.array([160,100,30])
-# upper_red = np.array([180,255,255])
+lower_red = np.array([0,130,135])
+upper_red = np.array([20,215,255])
 #green
 lower_green = np.array([40,100,30])
 upper_green = np.array([80,255,255])
@@ -97,17 +81,7 @@ while cap.isOpened():
     frame = cv2.GaussianBlur(frame, (5, 5), 2)
     #convert the frames to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #create a mask red
-    RMinH = cv2.getTrackbarPos('RMinH','image')
-    RMaxH = cv2.getTrackbarPos('RMaxH','image')
-    RMinS = cv2.getTrackbarPos('RMinS','image')
-    RMaxS = cv2.getTrackbarPos('RMaxS','image')
-    RMinV = cv2.getTrackbarPos('RMinV','image')
-    RMaxV = cv2.getTrackbarPos('RMaxV','image')
-    # lower_red = np.array([0,130,135])
-    # upper_red = np.array([20,215,255])
-    lower_red = np.array([RMinH,RMinS,RMinV])
-    upper_red = np.array([RMaxH,RMaxS,RMaxV])
+
     mask_red= cv2.inRange(hsv, lower_red, upper_red)
     #create a mask green
     mask_green= cv2.inRange(hsv, lower_green, upper_green)
@@ -141,7 +115,13 @@ while cap.isOpened():
     #find the contours for white
     frame = color_contorns(hsv, lower_white, upper_white, (255,255,255))
 
-    #show the frames
+    # stack horizontally 3 masks
+    mask = np.hstack((mask_red, mask_green, mask_blue))
+    mask2 = np.hstack((mask_white, mask_orange, mask_yellow))
+    mask3 = np.hstack((mask2, mask))
+
+    # show the masks
+    #cv2.imshow('mask', mask3)
     cv2.imshow('image', frame)
     # cv2.waitKey(1)
     #cv2.imshow('mask', mask)
