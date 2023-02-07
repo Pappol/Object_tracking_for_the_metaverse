90e5ee985c35aa68b5c14edf9c20bdbb63c5f294
Mattia2700 <mattia2700franzin+dev@gmail.com>
2023-02-07 17:15:04 +0100

    feat: side

--- object_tracking.py
+++ object_tracking.py
@@ -1,6 +1,33 @@
 import cv2
 import numpy as np
-import argparse
+
+def compute_rotation(red, green, blue, orange, yellow, white, frame):
+    #compute the total area
+    total_area = red + green + blue + orange + yellow + white
+    if total_area < 10:
+        return frame
+    #compute the percentage of each color
+    red_perc = red/total_area
+    green_perc = green/total_area
+    blue_perc = blue/total_area
+    orange_perc = orange/total_area
+    yellow_perc = yellow/total_area
+    white_perc = white/total_area
+    # green front, orange right, red left, blue back, yellow top, white bottom
+    # print text on frame based on the percentage of each color
+    if red_perc > 0.1:
+        cv2.putText(frame, 'LEFT', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    if green_perc > 0.1:
+        cv2.putText(frame, 'FRONT', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    if blue_perc > 0.1:
+        cv2.putText(frame, 'BACK', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    if orange_perc > 0.1:
+        cv2.putText(frame, 'RIGHT', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 165, 255), 2)
+    if yellow_perc > 0.1:
+        cv2.putText(frame, 'TOP', (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    if white_perc > 0.1:
+        cv2.putText(frame, 'BOTTOM', (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
 
 def dummy(x):
     pass
@@ -13,8 +40,6 @@ def circularity(cnt):
 def color_contorns(hsv, lower, upper, color):
 
     area = cv2.getTrackbarPos('area','image') / 100
-    length_l = cv2.getTrackbarPos('length_l','image')
-    length_u = cv2.getTrackbarPos('length_u','image')
     circ = cv2.getTrackbarPos('circularity','image') / 100
     #create a mask
     mask= cv2.inRange(hsv, lower, upper)
@@ -26,21 +51,15 @@ def color_contorns(hsv, lower, upper, color):
     # keep only 20% of the contours
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:int(len(contours)*area) if int(len(contours)*area) > 0 else 1]
     # filter by boundary length
-    contours = [cnt for cnt in contours if length_l < cv2.arcLength(cnt, True) < length_u]
+    contours = [cnt for cnt in contours if cv2.arcLength(cnt, True) > 100]
     # filter by circularity
     contours = [cnt for cnt in contours if circ < circularity(cnt)]
     #find the center of the contours
     for cnt in contours:
-        #filter for small areas
-        M = cv2.moments(cnt)
-        if M['m00'] != 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            #draw a circle on the center of the contours
-            cv2.circle(frame, (cx, cy), 10, color, -1)
-        #draw the contours
         cv2.drawContours(frame, contours, -1, color, 3)
-    return frame
+        area += cv2.contourArea(cnt)
+
+    return frame, area
 
 #name the window
 cv2.namedWindow('image')
@@ -48,31 +67,14 @@ cv2.namedWindow('image')
 cap = cv2.VideoCapture(0)
 
 cv2.createTrackbar('area','image',0,100, dummy)
-cv2.createTrackbar('length_u','image',0,400, dummy)
-cv2.createTrackbar('length_l','image',0,200, dummy)
 cv2.createTrackbar('circularity','image',0,100, dummy)
-cv2.setTrackbarPos('area','image',20)
-cv2.setTrackbarPos('length_l','image',10)
-cv2.setTrackbarPos('length_u','image',30)
-cv2.setTrackbarPos('circularity','image',40)
+cv2.setTrackbarPos('area','image',50)
+cv2.setTrackbarPos('circularity','image',45)
 
 #create a filter for different colors
 #red
-cv2.createTrackbar('RMinH','image',0,255, dummy)
-cv2.createTrackbar('RMaxH','image',0,255, dummy)
-cv2.createTrackbar('RMinS','image',0,255, dummy)
-cv2.createTrackbar('RMaxS','image',0,255, dummy)
-cv2.createTrackbar('RMinV','image',0,255, dummy)
-cv2.createTrackbar('RMaxV','image',0,255, dummy)
-cv2.setTrackbarPos('RMinH','image',0)
-cv2.setTrackbarPos('RMaxH','image',20)
-cv2.setTrackbarPos('RMinS','image',130)
-cv2.setTrackbarPos('RMaxS','image',215)
-cv2.setTrackbarPos('RMinV','image',135)
-cv2.setTrackbarPos('RMaxV','image',255)
-RMinH = RMaxH = RMinS = RMaxS = RMinV = RMaxV = 0
-# lower_red = np.array([160,100,30])
-# upper_red = np.array([180,255,255])
+lower_red = np.array([0,130,135])
+upper_red = np.array([20,215,255])
 #green
 lower_green = np.array([40,100,30])
 upper_green = np.array([80,255,255])
@@ -98,16 +100,6 @@ while cap.isOpened():
     #convert the frames to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #create a mask red
-    RMinH = cv2.getTrackbarPos('RMinH','image')
-    RMaxH = cv2.getTrackbarPos('RMaxH','image')
-    RMinS = cv2.getTrackbarPos('RMinS','image')
-    RMaxS = cv2.getTrackbarPos('RMaxS','image')
-    RMinV = cv2.getTrackbarPos('RMinV','image')
-    RMaxV = cv2.getTrackbarPos('RMaxV','image')
-    # lower_red = np.array([0,130,135])
-    # upper_red = np.array([20,215,255])
-    lower_red = np.array([RMinH,RMinS,RMinV])
-    upper_red = np.array([RMaxH,RMaxS,RMaxV])
     mask_red= cv2.inRange(hsv, lower_red, upper_red)
     #create a mask green
     mask_green= cv2.inRange(hsv, lower_green, upper_green)
@@ -129,17 +121,19 @@ while cap.isOpened():
     # mask_white = cv2.morphologyEx(mask_white, cv2.MORPH_CLOSE, kernel)
 
     #find the contours for red
-    frame = color_contorns(hsv, lower_red, upper_red, (0,0,255))
+    frame, red_area = color_contorns(hsv, lower_red, upper_red, (0,0,255))
     #find the contours for green
-    frame = color_contorns(hsv, lower_green, upper_green, (0,255,0))
+    frame, green_area = color_contorns(hsv, lower_green, upper_green, (0,255,0))
     #find the contours for blue
-    frame = color_contorns(hsv, lower_blue, upper_blue, (255,0,0))
+    frame, blue_area = color_contorns(hsv, lower_blue, upper_blue, (255,0,0))
     #find the contours for orange
-    frame = color_contorns(hsv, lower_orange, upper_orange, (0,165,255))
+    frame, orange_area = color_contorns(hsv, lower_orange, upper_orange, (0,165,255))
     #find the contours for yellow
-    frame = color_contorns(hsv, lower_yellow, upper_yellow, (0,255,255))
+    frame, yellow_area = color_contorns(hsv, lower_yellow, upper_yellow, (0,255,255))
     #find the contours for white
-    frame = color_contorns(hsv, lower_white, upper_white, (255,255,255))
+    frame, white_area = color_contorns(hsv, lower_white, upper_white, (255,255,255))
+
+    compute_rotation(red_area, green_area, blue_area, orange_area, yellow_area, white_area, frame)
 
     #show the frames
     cv2.imshow('image', frame)
